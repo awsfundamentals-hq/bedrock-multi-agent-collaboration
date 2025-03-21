70da697fe6a6d6f78ccd024e4b379e9b5d34fc4e
Tobias Schmidt <schmidt.tobias@protonmail.ch>
2025-03-21 09:02:36 +0100

    add bedrock sample agent

--- sst.config.ts
+++ sst.config.ts
@@ -12,5 +12,65 @@ export default $config({
   },
   async run() {
     new sst.aws.Nextjs('frontend');
+
+    const bedrockRole = new aws.iam.Role('bedrock-role', {
+      assumeRolePolicy: JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Action: ['sts:AssumeRole'],
+            Principal: {
+              Service: 'bedrock.amazonaws.com',
+            },
+          },
+        ],
+      }),
+      inlinePolicies: [
+        {
+          name: 'bedrockPolicy',
+          policy: JSON.stringify({
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Effect: 'Allow',
+                Action: [
+                  'bedrock:InvokeModel*',
+                  'bedrock:CreateInferenceProfile',
+                ],
+                Resource: [
+                  'arn:aws:bedrock:*::foundation-model/*',
+                  'arn:aws:bedrock:*:*:inference-profile/*',
+                  'arn:aws:bedrock:*:*:application-inference-profile/*',
+                ],
+              },
+              {
+                Effect: 'Allow',
+                Action: [
+                  'bedrock:GetInferenceProfile',
+                  'bedrock:ListInferenceProfiles',
+                  'bedrock:DeleteInferenceProfile',
+                  'bedrock:TagResource',
+                  'bedrock:UntagResource',
+                  'bedrock:ListTagsForResource',
+                ],
+                Resource: [
+                  'arn:aws:bedrock:*:*:inference-profile/*',
+                  'arn:aws:bedrock:*:*:application-inference-profile/*',
+                ],
+              },
+            ],
+          }),
+        },
+      ],
+    });
+    const bedrockAgent = new aws.bedrock.AgentAgent('bedrock-agent', {
+      agentName: 'test-agent-two',
+      agentResourceRoleArn: bedrockRole.arn,
+      idleSessionTtlInSeconds: 500,
+      foundationModel: 'us.anthropic.claude-3-5-sonnet-20241022-v2:0',
+      instruction:
+        'You are a helpful assistant specialized in AWS services. Your primary goal is to provide accurate, detailed information about AWS resources, best practices, and implementation strategies. You should always prioritize official AWS documentation when answering questions, and clearly indicate when you are uncertain about a specific detail. When users ask about code, provide complete, working examples with proper error handling and follow AWS security best practices.',
+    });
   },
 });
