996d1290a7632990a1f87943c9eb96735337b9c8
Tobias Schmidt <schmidt.tobias@protonmail.ch>
2025-03-23 11:22:07 +0100

    add more models

--- sst.config.ts
+++ sst.config.ts
@@ -4,6 +4,10 @@
 enum FoundationModels {
   Claude3_Haiku = 'anthropic.claude-3-haiku-20240307-v1:0',
   Claude3_5Sonnet = 'us.anthropic.claude-3-5-sonnet-20241022-v2:0',
+  Amazon_Nova_Lite = 'amazon.nova-lite-v1:0',
+  Amazon_Nova_Micro = 'amazon.nova-micro-v1:0',
+  Amazon_Titan_Text_Express = 'amazon.titan-text-express-v1',
+  Amazon_Titan_Text_Lite = 'amazon.titan-text-lite-v1',
 }
 
 export default $config({
